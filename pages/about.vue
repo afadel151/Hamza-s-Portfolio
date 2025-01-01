fd5492bf1201f6cc9ee989e7b662b016664af22e
afadel151 <af8356207@gmail.com>
2025-01-01 17:42:52 +0100

    finished about page responsivity with aos animations

--- pages/about.vue
+++ pages/about.vue
@@ -8,11 +8,11 @@
         <p
           class="text-5xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-br from-[#E58FFF] to-[#FFFFFF] font-bold">
           Professional <br> Web <br> Designer </p> -->
-        <p class="text-white md:px-0  text-lg text-center md:text-left font-thin" ref="finalText">
+        <p class="text-white md:px-0  text-lg text-justify md:text-left font-thin" ref="finalText">
           As a professional web designer, I specialize in creating visually compelling, user friendly websites that
           enhance both functionality and aesthetic appeal.
         </p>
-        <ul class=" text-gray-100 ml-10 font-thin flex flex-col space-y-2">
+        <ul class=" text-gray-100 ml-10 mt-5 font-thin flex flex-col space-y-2">
           <li class="flex items-center space-x-2">
             <p>●</p>
             <p>Design a custom, responsive website that works seamlessly on all devices.</p>
@@ -26,7 +26,7 @@
             <p> Boost brand identity through tailored visual designs and consistent branding.</p>
           </li>
         </ul>
-        <p class="text-white md:px-0  mt-5 text-lg text-center md:text-left font-thin">
+        <p class="text-white md:px-0  mt-5 text-lg text-justify md:text-left font-thin">
           By hiring me, you get a personalized, responsive website that enhances user experience, strengthens your
           brand, and drives real results
         </p>
@@ -37,7 +37,7 @@
     </div>
     <div class="px-[10%] md:px-[12%]  mb-20  mt-40 ">
       <p class="text-5xl font-bold text-gradient">Professional introduction</p>
-      <p class="text-lg font-thin text-gray-100 mt-10">
+      <p class="text-lg font-thin text-gray-100 mt-10 md:text-left  text-justify">
         Lorem ipsum dolor sit amet consectetur. Cursus diam et auctor justo. Curabitur ipsum tellus fames velit. Justo
         tristique Lorem ipsum dolor sit amet consectetur. Cursus diam et auctor justo. Curabitur ipsum tellus fames
         velit. Justo tristique Lorem ipsum dolor sit amet consectetur. Cursus diam et auctor justo. Curabitur ipsum
@@ -47,11 +47,11 @@
       </p>
     </div>
     <div class="px-[10%]  md:px-[12%]  h-fit   flex justify-between md:flex-row flex-col items-center  mb-20  mt-40 ">
-      <div class="flex h-[421px] w-[35%] flex-col justify-between items-start">
+      <div class="flex h-[421px] md:w-[35%] w-[80%] flex-col justify-between items-start">
 
         <div class="w-full">
           <p class="text-xl w-40 md:w-80  text-white font-thin">UI design</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full   relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[50%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -61,7 +61,7 @@
 
         <div class="w-full">
           <p class="text-xl w-40 md:w-80  text-white font-thin">User Research</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full  relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[50%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -72,7 +72,7 @@
         <div class="w-full">
 
           <p class="text-xl w-40 md:w-80  text-white font-thin">UX Design</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full  relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[50%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -83,7 +83,7 @@
         <div class="w-full">
 
           <p class="text-xl w-40 md:w-80  text-white font-thin">Prototyping</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full  relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[50%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -93,7 +93,7 @@
 
         <div class="w-full">
           <p class="text-xl w-40 md:w-80  text-white font-thin">Responsive Design</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full  relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[60%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -104,7 +104,7 @@
 
         <div class="w-full">
           <p class="text-xl w-40 md:w-80  text-white font-thin">Creativity</p>
-          <div class="md:w-full w-40 relative bg-white h-2 rounded-xl">
+          <div class="w-full  relative bg-white h-2 rounded-xl">
             <div data-aos="fade-right" data-aos-duration="1500"
               class="absolute h-2 w-[50%] bg-gradient-to-r from-violet-700 to-purple-500 rounded-full">
 
@@ -114,31 +114,31 @@
 
       </div>
       <div
-        class="w-[357px] flex justify-center items-center    h-[421px] z-10 bg-gradient-to-br from-[#5825C1] from-10% to-[#E858FF] rounded-lg shadow-custom-purple">
+        class="md:w-[357px] w-[82%] md:mt-0 mt-20 flex justify-center items-center h-[380px]     md:h-[421px] z-10 bg-gradient-to-br from-[#5825C1] from-10% to-[#E858FF] rounded-lg shadow-custom-purple">
 
 
-        <div class="grid grid-cols-2 gap-20">
-          <div
+        <div class="grid grid-cols-2 md:gap-20 gap-12">
+          <div data-aos="fade-right" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector.svg" alt="">
           </div>
-          <div
+          <div data-aos="fade-left" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector2.svg" alt="">
           </div>
-          <div
+          <div data-aos="fade-right" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector3.svg" alt="">
           </div>
-          <div
+          <div data-aos="fade-left" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector4.svg" alt="">
           </div>
-          <div
+          <div data-aos="fade-right" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector5.svg" alt="">
           </div>
-          <div
+          <div data-aos="fade-left" data-aos-duration="800"
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center bg-[#5825C1]">
             <img src="~/assets/imgs/Vector6.svg" alt="">
           </div>
@@ -147,7 +147,7 @@
     </div>
     <div class="px-[10%] md:px-[12%]   mb-20  mt-40 ">
       <p class="text-5xl font-bold text-gradient text-center">Career goals</p>
-      <div class="w-full text-center  grid gap-20 mt-20 grid-cols-2">
+      <div class="w-full md:text-center text-justify  grid gap-20 mt-20 md:grid-cols-2">
         <div>
           <p class="text-3xl font-bold text-[#9914C1]">Year 1</p>
           <ul class="text-white text-xl font-thin">
@@ -184,51 +184,54 @@
     </div>
     <div class="px-[10%] md:px-[12%]   mb-20  mt-40 ">
       <p class="text-5xl font-bold text-gradient text-center ">Hobbies and interests</p>
-      <div class="w-full mt-20 px-10 flex justify-between items-center">
+      <div class="w-full mt-20 px-10 md:flex md:gap-0 grid grid-cols-2 gap-10 md:justify-between md:items-center">
         <div class="flex flex-col items-center space-y-1">
           <div
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center ">
-            <Icon name="map:gym" style="color: #9913C1" size="2em" />
+            <Icon data-aos="fade-zoom-in" data-aos-easing="ease-in-back" name="map:gym" style="color: #9913C1"
+              size="2em" />
           </div>
           <p class="text-gradient">Gym</p>
         </div>
         <div class="flex flex-col items-center space-y-1">
           <div
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center ">
-            <Icon name="uil:plane-departure" style="color: #9913C1" size="2em" />
+            <Icon data-aos="fade-zoom-in" data-aos-easing="ease-in-back"  name="uil:plane-departure" style="color: #9913C1" size="2em" />
           </div>
           <p class="text-gradient">Traveling</p>
         </div>
         <div class="flex flex-col items-center space-y-1">
           <div
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center ">
-            <Icon name="hugeicons:equipment-gym-02" style="color: #9913C1" size="2em" />
+            <Icon data-aos="fade-zoom-in" data-aos-easing="ease-in-back"  name="hugeicons:equipment-gym-02" style="color: #9913C1" size="2em" />
           </div>
           <p class="text-gradient">Calisthenics</p>
         </div>
         <div class="flex flex-col items-center space-y-1">
           <div
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center ">
-            <Icon name="maki:swimming" style="color: #9913C1" size="2em" />
+            <Icon data-aos="fade-zoom-in" data-aos-easing="ease-in-back"  name="maki:swimming" style="color: #9913C1" size="2em" />
           </div>
           <p class="text-gradient">Swimming</p>
         </div>
         <div class="flex flex-col items-center space-y-1">
           <div
             class="w-16 h-16 rounded-full border shadow-custom-purple border-purple-400 flex justify-center items-center ">
-            <Icon name="mdi:movie-open" style="color: #9913C1" size="2em" />
+            <Icon data-aos="fade-zoom-in" data-aos-easing="ease-in-back"  name="mdi:movie-open" style="color: #9913C1" size="2em" />
           </div>
           <p class="text-gradient">Movies</p>
         </div>
       </div>
     </div>
-    <div class="px-[10%] md:px-[15%]  mb-20  mt-40 ">
-      <div class="px-10 bg-gradient-to-br flex justify-between items-center from-[#5825C1] from-[10%] rounded-3xl shadow-custom-purple w-full  to-[#E858FF] h-56">
-        <div class="w-5/6 flex flex-col justify-center space-y-10 h-full">
-          <p class="text-4xl text-white ">Let's connect and build something incredible!</p>
-          <p class="text-xl text-white font-thin ">Reach out to me for collaborations, inquiries, or just to say Hello! </p>
+    <div class="px-[10%] md:px-[15%]   mb-20  mt-40 ">
+      <div  data-aos="fade-up" data-aos-duration="1500"
+        class="md:px-10 bg-gradient-to-br md:py-0 py-10  animate-[bounce_2.5s_infinite] flex justify-between md:space-y-0 space-y-6 items-center md:flex-row flex-col from-[#5825C1] from-[10%] rounded-3xl shadow-custom-purple w-full  to-[#E858FF] md:h-52">
+        <div class="w-5/6 flex flex-col justify-center md:space-y-6 space-y-3 h-full">
+          <p class="md:text-4xl text-3xl text-white ">Let's connect and build something incredible!</p>
+          <p class="text-xl text-white font-thin ">Reach out to me for collaborations, inquiries, or just to say Hello!
+          </p>
         </div>
-        <div class="w-1/6">
+        <div class="md:w-1/6">
 
           <button class="button-gradient  z-10 ">Contact me </button>
         </div>
